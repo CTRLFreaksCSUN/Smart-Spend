4c24c529ed91efcc0eb005e7c309d908163cd903
Kenny <kennymunster5@gmail.com>
2025-02-28 18:33:34 -0800

    designed page

--- RegisterPage.php
+++ RegisterPage.php
@@ -7,22 +7,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="refresh" content="60">
     <title>Smart Spend(POC)-Register</title>
-      <link rel="stylesheet" href="Registerstyle.css">
+      <link rel="stylesheet" href="Registerstyle.css?v=<?php echo time(); ?>">
 </head>
 <body>
   <header>
-	<h1> <img src="images\SmartSpendLogo.png"
-	      alt="Smart Spend" style="width:90px; height:80px;"> Smart Spend</h1>
+  <h1> <img src="images\SmartSpendLogo.png"
+        alt="Smart Spend" style="width:90px; height:80px;"> Smart Spend</h1>
   </header>
+  <br>
+
+  <?php 
+  $path = __DIR__; 
+  include $path . '\validate_login.php';?>
+
  <div class="register">
-     <form> 
-        <label for="email">Enter new email: </label>
-	   <input type="text" id="email" name="email" placeholder="newuser123@gmail.com">
-	<label for="password">Enter new password: </label>
-	   <input type="password" id="password" name="password" placeholder="$Password1234">
-	<label for="password">Re-enter new password: </label>
-	   <input type="password" id="re-password" name="re-password" placeholder="$Password1234">
+     <form method="POST"> 
+  <h2>Sign Up</h2>
+  <span class="error"><?php echo $emailError?></span>
+    <br>
+   <br>
+  <label for="firstname" style="padding-right:18px;">Enter your First Name: </label>
+      <input type="text" id="firstname" name="firstname" placeholder="John"> <br>
+  <label for="middlename" style="padding-right:0px;">Enter your Middle Name: </label>
+      <input type="text" id="middlename" name="middlename" placeholder="[ Optional ]"> <br>
+  <label for="lastname" style="padding-right:20px;">Enter your Last Name: </label>
+      <input type="text" id="lastname" name="lastname" placeholder="Doe"> <br>
+        <label for="email" style="padding-right:60px;">Enter new Email: </label>
+     <input type="text" id="email" name="email" placeholder="newuser123@gmail.com"> <br>
+  <label for="password" style="padding-right:34px;">Enter new Password: </label>
+     <input type="password" id="password" name="password" placeholder="$Password1234"> <br>
+  <label for="password" style="padding-right:9px;">Re-enter new Password: </label>
+     <input type="password" id="re-password" name="re-password" placeholder="$Password1234"> <br>
+    <br>
+  <br>
+     <input type="submit" id="register" name="register" value="Sign Up">
+     </input>
      </form>
+  <form action="LoginPage.php" target="_self">
+     <input type="submit" id="login" name="login" value="Sign In"></input>
+ </form>
  </div>
   <footer>
       &copy; CTRL_Freaks - 2025
