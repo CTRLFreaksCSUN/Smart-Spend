337c359e547e9e2d7789900213a10b5091703ee1
Kenny <kennymunster5@gmail.com>
2025-03-04 16:31:33 -0800

    fixed syntax error

--- validate_login.php
+++ validate_login.php
@@ -1,46 +1,32 @@
 <?php
+use Aws\DynamoDb\Marshaler;
+use Aws\DynamoDb\DynamoDbClient;
+use Aws\DynamoDb\Exception\DynamoDbException;
+use DynamoDb\DynamoDBAttribute;
+use DynamoDb\DynamoDBService;
+
 use Dotenv\Dotenv;
 require __DIR__.'/vendor/autoload.php';
 $env = Dotenv::createImmutable(__DIR__);
 $env->load();
 
 $email = $password = "";
-$emailError = $passwordError = "";
+$errorMsg = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($_POST["email"])) {
-     $emailError = "Please enter your email.";
+     $errorMsg = "*Please enter your email.*";
   }
 
   else {
-     $email = scan_input($_POST["email"]);
-  }
-  
-  if (empty($_POST["password"])) {
-     $passwordError = "Please enter your password.";
-  }
+      $email = scan_input($_POST["email"]);
+      if (empty($_POST["password"])) {
+      $errorMsg = "*Please enter your password.*";
+      }
 
-  else {
-     $password = scan_input($_POST["password"]);
-  }
-}
-
-$host = $_ENV["HOST"];
-$dUser = $_ENV["USER"];
-$dPassw =  $_ENV["PASSWORD"];
-
-$connect = new mysqli($host, $dUser, $dPassw);
-if ($connect->connect_error) {
-   die("Failed to connect to database" . $connect->connect_error);
-}
-
-echo "Connected to database";
-mysqli_close($connect);
-
-function scan_input($input) {
-  $input = trim($input);
-  $input = stripslashes($input);
-  $input = htmlspecialchars($input);
-  return $input;
+      else {
+         $password = scan_input($_POST["password"]);
+      }
+   }
 }
 ?>
